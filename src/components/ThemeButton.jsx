845ce23fc3deb952023b4dec6dbb3a797da32451
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-04-10 21:54:27 +0530

    Create theme change functionality

---
+++ src/components/ThemeButton.jsx
@@ -0,0 +1,16 @@
+import {useTheme} from "../hooks/ThemeProvider.jsx";
+import {ThemeIcons} from "../data/ThemeIcons.jsx";
+
+const ThemeButton = () => {
+  const {currentTheme, toggleTheme} = useTheme();
+
+  return (
+    <>
+      <button className="h-fit w-fit rounded-md" onClick={toggleTheme}>
+        {currentTheme === "light" ? ThemeIcons.light : ThemeIcons.dark}
+      </button>
+    </>
+  );
+};
+
+export default ThemeButton;

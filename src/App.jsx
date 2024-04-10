83bcc8d299955c647b78e47da15285d542aa25e0
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-04-10 19:13:51 +0530

    Create 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Error404 from "./pages/404.jsx";
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path={"/"} element={<Error404/>}/>
+        <Route path={"*"} element={<Error404/>}/>
+      </Routes>
     </>
   )
 }

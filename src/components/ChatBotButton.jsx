27662af9ade1b2cc960130dbe32355b9ce063895
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-04-10 23:41:06 +0530

    Create ChatBotButton

---
+++ src/components/ChatBotButton.jsx
@@ -0,0 +1,12 @@
+import {FaRobot} from "react-icons/fa";
+
+const ChatBotButton = () => {
+  return (
+    <button
+      className="fixed right-0 bottom-12 m-6 bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded-full">
+      <FaRobot className="h-8 w-8"/>
+    </button>
+  );
+};
+
+export default ChatBotButton;

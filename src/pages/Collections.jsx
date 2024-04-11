5daa8e52f29a97f9948255e68c792736eece1097
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-04-12 00:08:18 +0530

    Improvements

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -18,7 +18,8 @@ const Collections = () => {
           </div>
 
           <div className="mx-auto max-w-2xl py-10 lg:max-w-none lg:py-12">
-            <h2 className="text-2xl font-bold text-gray-900">Men&apos;s Collection</h2>
+            <h2 className="text-2xl font-bold text-gray-900 dark:text-white transition duration-500">Men&apos;s
+              Collection</h2>
 
             <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
               {collections.map((callout) => (
@@ -31,20 +32,22 @@ const Collections = () => {
                       className="h-full w-full object-cover object-center"
                     />
                   </div>
-                  <h3 className="mt-6 text-sm text-gray-500">
+                  <h3 className="mt-6 text-sm text-gray-500 dark:text-gray-500 transition duration-500">
                     <a href={callout.href}>
                       <span className="absolute inset-0"/>
                       {callout.name}
                     </a>
                   </h3>
-                  <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                  <p
+                    className="text-base font-semibold text-gray-900 dark:text-white transition duration-500">{callout.description}</p>
                 </div>
               ))}
             </div>
           </div>
 
           <div className="mx-auto max-w-2xl py-10 lg:max-w-none lg:py-12">
-            <h2 className="text-2xl font-bold text-gray-900">Women&apos;s Collection</h2>
+            <h2 className="text-2xl font-bold text-gray-900 dark:text-white transition duration-500">Women&apos;s
+              Collection</h2>
 
             <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
               {collections.map((callout) => (
@@ -57,13 +60,14 @@ const Collections = () => {
                       className="h-full w-full object-cover object-center"
                     />
                   </div>
-                  <h3 className="mt-6 text-sm text-gray-500">
+                  <h3 className="mt-6 text-sm text-gray-500 dark:text-gray-500 transition duration-500">
                     <a href={callout.href}>
                       <span className="absolute inset-0"/>
                       {callout.name}
                     </a>
                   </h3>
-                  <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                  <p
+                    className="text-base font-semibold text-gray-900 dark:text-white transition duration-500">{callout.description}</p>
                 </div>
               ))}
             </div>

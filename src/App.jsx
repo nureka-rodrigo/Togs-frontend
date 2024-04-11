869dc37a7d7f5ef96aeaf70e48ffa5220fd6438b
Nethma Kalpani <Kalpani4lk@gmail.com>
2024-04-11 20:04:22 +0530

    Create Contact page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Error404 from "./pages/404.jsx";
 import Home from "./pages/Home.jsx";
+import Contact from './pages/Contact.jsx';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <div className="bg-white dark:bg-gray-950 transition duration-500">
         <Routes>
           <Route path={"/"} element={<Home/>}/>
+          <Route path={"/contact"} element={<Contact/>}/>
           <Route path={"*"} element={<Error404/>}/>
         </Routes>
       </div>
